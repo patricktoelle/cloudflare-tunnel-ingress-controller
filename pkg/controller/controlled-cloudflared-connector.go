5442a8e44b3b8fbf4db72750b2bcf6b3a751e1b0
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-07 16:54:07 -0600

    Refactor to just forward controller volume mounts
    I gave this some more thought and I think just letting consumers mount volumes to the controller and then forward those to the cloudflared pod would be better. Curious what you think!

--- pkg/controller/controlled-cloudflared-connector.go
+++ pkg/controller/controlled-cloudflared-connector.go
@@ -11,22 +11,16 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-type CaPool struct {
-	ConfigMap *struct{ Name string }
-	Secret    *struct{ Name string }
-	Key       string
-	MountPath string
-}
 
 func CreateControlledCloudflaredIfNotExist(
 	ctx context.Context,
 	kubeClient client.Client,
 	tunnelClient *cloudflarecontroller.TunnelClient,
 	namespace string,
-	caPool *CaPool,
 ) error {
 	list := appsv1.DeploymentList{}
 	err := kubeClient.List(ctx, &list, &client.ListOptions{
@@ -43,43 +37,25 @@ func CreateControlledCloudflaredIfNotExist(
 		return nil
 	}
 
-	var caPoolSource *v1.VolumeSource = nil
-	if caPool.ConfigMap != nil && caPool.Secret != nil {
-		return errors.New("Only one of --capool-config-map or --capool-secret may be specified")
-	} else if caPool.ConfigMap != nil {
-		caPoolSource = &v1.VolumeSource{
-			ConfigMap: &v1.ConfigMapVolumeSource{
-				LocalObjectReference: v1.LocalObjectReference{
-					Name: caPool.ConfigMap.Name,
-				},
-			},
-		}
-	} else if caPool.Secret != nil {
-		caPoolSource = &v1.VolumeSource{
-			Secret: &v1.SecretVolumeSource{
-				SecretName: caPool.Secret.Name,
-			},
-		}
-	}
-
-	var extraVolumes []v1.Volume
-	var extraVolumeMounts []v1.VolumeMount
-
-	if caPoolSource != nil {
-		extraVolumes = append(extraVolumes, v1.Volume{
-			Name:         "ca-pool",
-			VolumeSource: *caPoolSource,
-		})
-		extraVolumeMounts = append(extraVolumeMounts, v1.VolumeMount{
-			Name:      "ca-pool",
-			MountPath: caPool.MountPath,
-			SubPath:   caPool.Key,
-		})
-	}
-
 	token, err := tunnelClient.FetchTunnelToken(ctx)
 	if err != nil {
 		return errors.Wrap(err, "fetch tunnel token")
+	}
+
+	controllerPod := v1.Pod{}
+	err = kubeClient.Get(ctx, types.NamespacedName{
+		Namespace: namespace,
+		Name:      os.Getenv("POD_NAME"),
+	}, &controllerPod)
+	if err != nil {
+		return errors.Wrap(err, "get controller pod")
+	}
+
+	var controllerVolumeMounts []v1.VolumeMount
+	for _, container := range controllerPod.Spec.Containers {
+		if container.Name == "cloudflare-tunnel-ingress-controller" {
+			controllerVolumeMounts = container.VolumeMounts
+		}
 	}
 
 	replicas, err := strconv.ParseInt(os.Getenv("CLOUDFLARED_REPLICA_COUNT"), 10, 32)
@@ -87,37 +63,18 @@ func CreateControlledCloudflaredIfNotExist(
 		return errors.Wrap(err, "invalid replica count")
 	}
 
-	deployment := cloudflaredConnectDeploymentTemplating(token, namespace, int32(replicas), extraVolumes, extraVolumeMounts)
+	deployment := cloudflaredConnectDeploymentTemplating(
+		token,
+		namespace,
+		controllerPod.Spec.Volumes,
+		controllerVolumeMounts,
+	)
+
 	err = kubeClient.Create(ctx, deployment)
 	if err != nil {
 		return errors.Wrap(err, "create controlled-cloudflared-connector deployment")
 	}
 	return nil
-}
-
-func GetCaPoolOptions(configMap, secret, key, mountPath *string) *CaPool {
-	var caPool *CaPool = nil
-	if configMap != nil || secret != nil {
-		caPool = &CaPool{
-			Key:       "ca-certificates.crt",
-			MountPath: "/etc/ssl/certs",
-		}
-
-		if configMap != nil {
-			caPool.ConfigMap = &struct{ Name string }{Name: *configMap}
-		}
-		if secret != nil {
-			caPool.Secret = &struct{ Name string }{Name: *secret}
-		}
-		if key != nil {
-			caPool.Key = *key
-		}
-		if mountPath != nil {
-			caPool.MountPath = *mountPath
-		}
-	}
-
-	return caPool
 }
 
 func cloudflaredConnectDeploymentTemplating(
@@ -170,11 +127,11 @@ func cloudflaredConnectDeploymentTemplating(
 								"--token",
 								token,
 							},
-							VolumeMounts: extraVolumeMounts,
+							VolumeMounts: volumeMounts,
 						},
 					},
 					RestartPolicy: v1.RestartPolicyAlways,
-					Volumes:       extraVolumes,
+					Volumes:       volumes,
 				},
 			},
 		},
